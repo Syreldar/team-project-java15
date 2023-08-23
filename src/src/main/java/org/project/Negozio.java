0f9b26649f8f66a2a9f9d90333ccc293ffcbe2eb
Sara <saracoppola996@gmail.com>
2023-08-23 14:27:46 +0200

    aggiunto branch shop

---
+++ src/src/main/java/org/project/Negozio.java
@@ -0,0 +1,34 @@
+package org.project;
+
+public class Negozio {
+    private String name;
+    private String owner;
+    private String typeOfShop;
+    private static final int minCapacity = 0;
+    private static final int maxCapacity = 100;
+    private static int numberOfShops;
+
+    private static void keepTrackOfShopNumber() {numberOfShops ++;}
+
+    public Negozio(String name, String owner, String typeOfShop){
+        this.name = name;
+        this.owner = owner;
+        this.typeOfShop= typeOfShop;
+
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getOwner(){
+        return owner;
+    }
+
+    public String getTypeOfShop(){
+        return typeOfShop;
+    }
+
+
+
+}
