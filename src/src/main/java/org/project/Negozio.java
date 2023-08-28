9aa37065907affd47b8cfaad9d41bb6e86dea7ba
Sara <saracoppola996@gmail.com>
2023-08-28 17:25:46 +0200

    modifica classe Negozio

--- src/src/main/java/org/project/Negozio.java
+++ src/src/main/java/org/project/Negozio.java
@@ -1,34 +1,31 @@
 package org.project;
 
 public class Negozio {
-    private String name;
-    private String owner;
-    private String typeOfShop;
+    private String nome;
+    private double venditeTotali;
+    private String proprietario;
+    private String categoriaPiuVenduta;
     private static final int minCapacity = 0;
-    private static final int maxCapacity = 100;
+    private static final int maxCapacity = 2000;
     private static int numberOfShops;
 
     private static void keepTrackOfShopNumber() {numberOfShops ++;}
 
-    public Negozio(String name, String owner, String typeOfShop){
-        this.name = name;
-        this.owner = owner;
-        this.typeOfShop= typeOfShop;
-
+    public Negozio(String nome, double venditeTotali, String categoriaPiuVenduta){
+        this.nome = nome;
+        this.venditeTotali = venditeTotali;
+        this.categoriaPiuVenduta = categoriaPiuVenduta;
     }
 
-    public String getName(){
-        return name;
+    public String getNome(){
+        return nome;
     }
 
-    public String getOwner(){
-        return owner;
+    public String getCategoriaPiuVenduta(){
+        return categoriaPiuVenduta;
     }
 
-    public String getTypeOfShop(){
-        return typeOfShop;
+    public double getVenditeTotali() {
+        return venditeTotali;
     }
-
-
-
 }
