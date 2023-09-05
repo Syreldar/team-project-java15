933e715ba7d2e9a3460444483b8b75f4201b154c
Claudio Vittorio Carta <claudio.vittorio.carta@gmail.com>
2023-09-05 13:21:10 +0200

    added class Database and class Product

---
+++ src/main/java/org/project/Database.java
@@ -0,0 +1,29 @@
+
+
+import org.project.Customer;
+import org.project.Shop;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+public class Database {
+    List<Shop> shops;
+    List<Customer> customers;
+    HashMap<Shop, List<Product>>sales;
+    public void addSales(Shop sellerShop, Product product) {
+        if(!sales.containsKey(sellerShop)) {
+            sales.put(sellerShop, new ArrayList<Product>());
+        }
+        sales.get(sellerShop).add(product);
+    }
+    public Database(){
+        this.shops = new ArrayList<Shop>();
+    }
+    public void addShop(Shop shop) {
+        shops.add(shop);
+    }
+    public void addCustomer(Customer customer) {
+        customers.add(customer);
+    }
+}
---
+++ src/main/java/org/project/Product.java
@@ -0,0 +1,30 @@
+public class Product {
+
+    String name;
+    String manufacturer;
+    double price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
