b4342a908727093ec1cd5e1041d72f454111d2eb
Andrea Zora <zora.andrea93@gmail.com>
2023-10-10 14:45:35 +0200

    Added information about shops, products, and customers to the database.

--- make_table.sql
+++ make_table.sql
@@ -5,6 +5,12 @@ CREATE TABLE Shops (
     owner_name VARCHAR(50)
 );
 
+INSERT INTO Shops (name, owner_name)
+VALUES
+    ('ShopA', 'Carlo'),
+    ('ShopB', 'Para'),
+    ('ShopC', 'Malo');
+
 -- Products Table
 CREATE TABLE Products (
     id INT PRIMARY KEY AUTO_INCREMENT,
@@ -15,6 +21,27 @@ CREATE TABLE Products (
     stock INT,
     FOREIGN KEY (shop_id) REFERENCES Shops(id)
 );
+
+-- ShopA
+INSERT INTO Products (shop_id, category, name, price, stock)
+VALUES
+    (1, 'ELECTRONICS', 'Computer', 500.00, 7),
+    (1, 'CLEANING', 'Detergent', 10.00, 21),
+    (1, 'FOOD', 'Apple', 2.20, 231);
+
+-- ShopB
+INSERT INTO Products (shop_id, category, name, price, stock)
+VALUES
+    (2, 'ELECTRONICS', 'Computer', 550.00, 6),
+    (2, 'HEALTH', 'Medicine', 10.00, 78),
+    (2, 'FOOD', 'Banana', 2.10, 194);
+
+-- ShopC
+INSERT INTO Products (shop_id, category, name, price, stock)
+VALUES
+    (3, 'CLEANING', 'Detergent', 12.00, 24),
+    (3, 'HEALTH', 'Medicine', 11.00, 82),
+    (3, 'FOOD', 'Pear', 2.50, 253);
 
 -- Customers Table
 CREATE TABLE Customers (
