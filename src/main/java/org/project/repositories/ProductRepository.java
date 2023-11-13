c6d210ae5bd40cbcea95e8b5c5b27f897adfd54c
Sara <saracoppola996@gmail.com>
2023-11-13 17:43:06 +0100

    Shop and Product Repository added

---
+++ src/main/java/org/project/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package org.project.repositories;
+
+import org.project.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
---
+++ src/main/java/org/project/repositories/ShopRepository.java
@@ -0,0 +1,9 @@
+package org.project.repositories;
+
+import org.project.models.Shop;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ShopRepository extends JpaRepository<Shop, Long> {
+}
